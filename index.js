70835a37356a22a2739712eb8c307d8b8f37271a
Arsen <Arsenaliakparov5@gmail.com>
2025-01-28 11:48:16 +0700

    added ooo

--- index.js
+++ index.js
@@ -4,3 +4,16 @@ box.innerHTML = "Hello Aidai";
 let content = document.querySelector(".content")
 
 content.innerHTML = "Hello Arsen";
+
+let ooo = document.getElementById("ooo")
+let ppp = document.getElementById("ppp")
+
+
+function rrr(){
+        // ooo.innerHTML = timer
+        ooo.style.beckgroundColor = "red"
+        ppp.style.beckgroundColor = "green"
+    
+}
+ooo.onclick = rrr
+ppp.onclick = rrr
